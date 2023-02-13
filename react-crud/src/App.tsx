088819a807678d7a45916daebfbb7f9c81b6be6b
crackslinger <fbueller2234@protonmail.com>
2023-02-13 01:48:20 +0100

    better routing, working fetch from admin

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,68 +1,24 @@
 import React from 'react';
+//import { render } from 'react-dom';
 import './App.css';
+import Products from "./admin/Products";
+import Main from "./main/Main";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-    return (
-      <div className="App">
-
-        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
-          <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap">
-              <a className="nav-link" href="#">Sign out</a>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="container-fluid">
-          <div className="row">
-            <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-              <div className="sidebar-sticky pt-3">
-                <ul className="nav flex-column">
-                  <li className="nav-item">
-                    <a className="nav-link active" href="#">
-                    Dashboard 
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-
-            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-
-
-              <h2>Section title</h2>
-              <div className="table-responsive">
-                <table className="table table-striped table-sm">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Header</th>
-                      <th>Header</th>
-                      <th>Header</th>
-                      <th>Header</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1,001</td>
-                      <td>Lorem</td>
-                      <td>ipsum</td>
-                      <td>dolor</td>
-                      <td>sit</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    )
+  return (
+  //  render (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element ={<Main />}/>
+          <Route path='/admin/products' element ={<Products />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+    
+    //)
+  )
 };
 
 
---
+++ react-crud/src/admin/Products.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useEffect } from 'react';
+import Wrapper from './Wrapper';
+
+const Products = () => {
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch('http://172.18.0.1:8000/api/products');
+      const data = await response.json();
+
+      console.log(data);
+    }
+    getProducts();
+  }, []);  
+
+  return (
+    <Wrapper>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Header</th>
+              <th>Header</th>
+              <th>Header</th>
+              <th>Header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td>
+              <td>Lorem</td>
+              <td>ipsum</td>
+              <td>dolor</td>
+              <td>sit</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Products;
